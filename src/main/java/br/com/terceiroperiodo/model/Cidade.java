68102086749eced411dea44a447573c4621d6e60
Ricardo Prado <ricardofprado98@gmail.com>
2023-05-09 19:14:06 -0300

    Alteração aula terça

--- src/main/java/br/com/terceiroperiodo/model/Cidade.java
+++ src/main/java/br/com/terceiroperiodo/model/Cidade.java
@@ -1,8 +1,20 @@
 package br.com.terceiroperiodo.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Cidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     String nome;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_estado")
     Estado estado;
 
 }
